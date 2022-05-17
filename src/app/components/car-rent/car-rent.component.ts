b26d81a547cbf20526587bc070604927ed9f7f31
Merve Esen <merveesen834@gmail.com>
2022-05-17 16:38:11 +0300

    Design improved

--- src/app/components/car-rent/car-rent.component.ts
+++ src/app/components/car-rent/car-rent.component.ts
@@ -1,15 +1,93 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { Rental } from 'src/app/models/rental';
+import { User } from 'src/app/models/user';
+import { RentalService } from 'src/app/services/rental.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-car-rent',
   templateUrl: './car-rent.component.html',
-  styleUrls: ['./car-rent.component.css']
+  styleUrls: ['./car-rent.component.css'],
+  providers: [RentalService]
 })
+
 export class CarRentComponent implements OnInit {
 
-  constructor() { }
+  rental: Rental;
+  carId: number;
+  addRentCarForm: FormGroup;
+  isLoggedIn: Observable<boolean>;
+  currentUser: User;
+  currentDate: Date = new Date();
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private rentalService: RentalService,
+    private authService: AuthService,
+    private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.loginStatus;
+    this.isLoggedIn.subscribe(() => {  //if logged in
+      this.getCurrentUser();
+    })
+    this.carId = parseInt(this.activatedRoute.snapshot.params['carId']);
+    this.createAddRentCarForm();
+  }
+
+  createAddRentCarForm() {
+    this.addRentCarForm = this.formBuilder.group({
+      carId: [this.carId, Validators.required],
+      customerId: [this.currentUser.id, Validators.required],
+      rentDate: ['', [Validators.required]],
+      returnDate: ['', Validators.required]
+    });
+  }
+
+  getCurrentUser() {
+    this.currentUser = this.authService.getUser()!;
+  }
+
+  rent() {
+    if (this.addRentCarForm.invalid) {
+      this.toastrService.warning('Alanları kontrol ediniz', 'Dikkat');
+      return;
+    }
+
+    this.rental = this.addRentCarForm.value;
+    let rentDate = new Date(this.rental.rentDate);
+    let returnDate = new Date(this.rental.returnDate);
+
+    if (rentDate < this.currentDate) {
+      this.toastrService.warning(
+        'Kiralama tarihi, bu günden sonraki günler olmalıdır', 'Dikkat'
+      );
+      return;
+    }
+
+    if (returnDate < rentDate || returnDate.getDate() == rentDate.getDate()) {
+      this.toastrService.warning(
+        'Teslim tarihi, kiralama tarihinden sonraki günler olmalıdır', 'Dikkat'
+      );
+      return;
+    }
+
+    this.rentalService.isRentable(this.rental).subscribe(response => {
+      if (!response.success) {
+        this.toastrService.warning(
+          'Bu aracı bu tarihler arasında kiralayamazsınız', 'Dikkat'
+        );
+        return;
+      }
+
+      this.toastrService.success('Ödeme sayfasına yönlendiriliyorsunuz');
+      return this.router.navigate(['/cards']);
+    });
+  }
 }
